2db673c6d5a3f6198a1d8e7d4004cf8da0808533
elineelorm <elinenuviadenu38@gmail.com>
2022-10-24 23:59:26 -0400

    Add hashing

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -26,8 +27,8 @@ const userSchema = new mongoose.Schema({
 });
 
 
-//only encrypt passwords cause you will want to search the emails
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+// //only encrypt passwords cause you will want to search the emails
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -48,7 +49,8 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        // password: req.body.password
+        pasword: md5(req.body.password)
     });
 
     newUser.save(function(err){
@@ -62,7 +64,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, function(err, foundUser){
         if (err){
